affc8e3e05c1ceede8214987827688d86f0fca89
xueshanyinshi <729119495@qq.com>
2017-09-22 13:35:52 +0800

    cd
    vv

---
+++ WKTForWKT/WKTForWKT/Class1.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WKTForWKT
+{
+    public class Class1
+    {
+        [CommandMethod("MKENTS")]
+        public void MKENTS()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            Transaction trans = doc.TransactionManager.StartTransaction();
+            try
+            {
+                PromptResult pr = ed.GetString("输入WKT字符串");
+                if(pr.Status == PromptStatus.OK)
+                {
+                    string wktStr = pr.StringResult;
+
+                }
+
+
+
+            }
+            catch
+            {
+
+
+            }
+            finally
+            {
+                trans.Dispose();
+            }
+        }
+    }
+}
---
+++ WKTForWKT/WKTForWKT/wktForAutocad.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using GeoAPI.Geometries;
+using NetTopologySuite.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WKTForWKT
+{
+    public static class WKTForAutocad
+    {
+        public static Entity WKTToAutoCADGeometry(this string WKTStr)
+        {
+            WKTReader reader = new WKTReader();
+            IGeometry geom = reader.Read(WKTStr);
+            switch (geom.OgcGeometryType)
+            {
+                case OgcGeometryType.Point:
+                    return FromPoint((IPoint)geometry, factory, setUserData);
+                case OgcGeometryType.LineString:
+                    return FromLineString((ILineString)geometry, factory, setUserData);
+                case OgcGeometryType.Polygon:
+                    return FromPolygon((IPolygon)geometry, factory, setUserData);
+                case OgcGeometryType.MultiPoint:
+                    return FromMultiPoint((IMultiPoint)geometry, factory, setUserData);
+                case OgcGeometryType.MultiLineString:
+                    return FromMultiLineString((IMultiLineString)geometry, factory, setUserData);
+                case OgcGeometryType.MultiPolygon:
+                    return FromMultiPolygon((IMultiPolygon)geometry, factory, setUserData);
+                case OgcGeometryType.GeometryCollection:
+                    return FromGeometryCollection((IGeometryCollection)geometry, factory, setUserData);
+                default:
+                    throw new ArgumentException();
+            }
+
+            return null;
+        }
+
+    }
+}
